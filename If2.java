d59401650244164e8280b62f98978912651b63bc
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-04-30 23:29:40 +0530

    Add files via upload

---
+++ If2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class  If2
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Are You A Paid Student?");
+		boolean ip = sc.nextBoolean();
+		
+		if(ip)
+		{
+			System.out.println("Pay The Fees");
+			
+			
+		}
+		System.out.println("Attend The Class");
+	}
+}
