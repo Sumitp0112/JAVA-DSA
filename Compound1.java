3e7f1de0c8e21a11d4aa61a894121d8f82ca87fe
Sumit Patil <145496112+Sumitp0112@users.noreply.github.com>
2025-04-30 23:22:42 +0530

    Add files via upload

---
+++ Compound1.java
@@ -0,0 +1,32 @@
+class Compound1 
+{
+	public static void main(String[] args) 
+	{
+		int sal = 4000;
+		
+		//sal +=200;
+		//System.out.println(sal);
+		
+		//sal -=200;
+		//System.out.println(sal);
+		
+		//sal *=3;
+		//System.out.println(sal);
+		
+		//sal /=2;
+		//System.out.println(sal);
+		
+		//sal %=2;
+		//System.out.println(sal);
+		
+		sal +=2000.50;
+		System.out.println(sal);
+		
+		byte b = 127;
+		b +=100;
+		System.out.println(b);
+		
+		
+		
+	}
+}
